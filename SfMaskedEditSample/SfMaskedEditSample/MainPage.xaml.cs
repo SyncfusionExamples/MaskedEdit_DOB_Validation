23eed4ce069ec585fc93fea83bd12ac4ff70e6bb
sheik syed abthaheer <sheiksyedm@syncfusion.com>
2019-10-04 16:22:52 +0530

    Sample files added.

---
+++ SfMaskedEditSample/SfMaskedEditSample/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using Xamarin.Forms;
+
+namespace SfMaskedEditSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
